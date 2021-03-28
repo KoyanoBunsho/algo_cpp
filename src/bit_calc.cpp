0f65c25ccae729fcd98aa10107ff022ccb3be6eb
KoyanoBunsho <buna40950@gmail.com>
2021-03-28 20:17:49 +0900

    bit

---
+++ src/bit_calc.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a = 45;
+    int b = 25;
+    cout << a << " AND " << b << " = " << (a&b) << endl;
+    cout << a << " AND " << b << " = " << (a|b) << endl;
+
+    return 0;
+}
