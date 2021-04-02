5af526241385f5fdec15f60704ff349ce0794a3a
KoyanoBunsho <buna40950@gmail.com>
2021-04-02 19:17:00 +0900

    num

---
+++ src/chap3/pow_practice.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long modpow(long long a, long long n, long long mod){
+    long long res = 1;
+    while(n>0){
+        if(n&1)res = res*a%mod;
+        a = a*a%mod;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    cout << modpow(3, 45, 1000000007) << endl;
+    return 0;
+}
