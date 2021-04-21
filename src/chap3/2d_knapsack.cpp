5f67f508a4f1467245d87ed05861e1f278174f07
KoyanoBunsho <buna40950@gmail.com>
2021-04-21 16:04:57 +0900

    dp

--- src/chap3/2d_knapsack.cpp
+++ src/chap3/2d_knapsack.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for(int i=0; i<n; i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
 
 int dp[100010][100010];
-int main(){
-    int n, W;
-    cin >> n >> W;
-    vector<int> w(n), v(n);
-    rep(i, n) cin >> v[i] >> w[i];
-    rep(i, W+1)dp[0][i] = 0;
-    rep(i, n){
-        rep(j, W+1){
-            if(j>=w[i]){
-                dp[i+1][j] = max(dp[i][j], dp[i][j-w[i]]+v[i]);
-            }else{
-                dp[i+1][j] = dp[i][j];
-            }
-        }
+int main() {
+  int n, W;
+  cin >> n >> W;
+  vector<int> w(n), v(n);
+  rep(i, n) cin >> v[i] >> w[i];
+  rep(i, W + 1) dp[0][i] = 0;
+  rep(i, n) {
+    rep(j, W + 1) {
+      if (j >= w[i]) {
+        dp[i + 1][j] = max(dp[i][j], dp[i][j - w[i]] + v[i]);
+      } else {
+        dp[i + 1][j] = dp[i][j];
+      }
     }
-    cout << dp[n][W] << endl;
-    return 0;
+  }
+  cout << dp[n][W] << endl;
+  return 0;
 }
