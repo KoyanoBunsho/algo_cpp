6d28e7dc54c763a8b33151e2a3848dead42920bd
KoyanoBunsho <buna40950@gmail.com>
2021-04-22 17:20:30 +0900

    trie tree

--- src/chap3/trie_tree.cpp
+++ src/chap3/trie_tree.cpp
@@ -1,52 +1,52 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for(int i=0; i<n; i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
 
 const int ALPHABET_SIZE = 26;
 
-struct TrieNode{
-    struct TrieNode *children[ALPHABET_SIZE];
-    bool isEndOfWord;
+struct TrieNode {
+  struct TrieNode *children[ALPHABET_SIZE];
+  bool isEndOfWord;
 };
 
-struct TrieNode *getNode(void){
-    struct TrieNode *pNode = new TrieNode;
-    pNode->isEndOfWord = false;
-    rep(i, ALPHABET_SIZE){
-        pNode->children[i] = NULL;
-    }
-    return pNode;
+struct TrieNode *getNode(void) {
+  struct TrieNode *pNode = new TrieNode;
+  pNode->isEndOfWord = false;
+  rep(i, ALPHABET_SIZE) { pNode->children[i] = NULL; }
+  return pNode;
 }
 
-void insert(struct TrieNode *root, string key){
-    struct TrieNode *pCrawl = root;
-    rep(i, key.length()){
-        int index = key[i] - 'a';
-        if(!pCrawl->children[index]){
-            pCrawl->children[index] = getNode();
-        }
-        pCrawl = pCrawl->children[index];
+void insert(struct TrieNode *root, string key) {
+  struct TrieNode *pCrawl = root;
+  rep(i, key.length()) {
+    int index = key[i] - 'a';
+    if (!pCrawl->children[index]) {
+      pCrawl->children[index] = getNode();
     }
-    pCrawl->isEndOfWord = true;
+    pCrawl = pCrawl->children[index];
+  }
+  pCrawl->isEndOfWord = true;
 }
 
-bool search(struct TrieNode *root, string key){
-    struct TrieNode *pCrawl = root;
-    rep(i, key.length()){
-        int index = key[i] - 'a';
-        if(!pCrawl->children[index]) return false;
-        pCrawl = pCrawl->children[index];
-    }
-    return (pCrawl != NULL && pCrawl->isEndOfWord);
+bool search(struct TrieNode *root, string key) {
+  struct TrieNode *pCrawl = root;
+  rep(i, key.length()) {
+    int index = key[i] - 'a';
+    if (!pCrawl->children[index])
+      return false;
+    pCrawl = pCrawl->children[index];
+  }
+  return (pCrawl != NULL && pCrawl->isEndOfWord);
 }
 
-int main(){
-    string keys[] = {"the", "a", "there",
-    "answer", "any", "by", "bye", "their"};
-    int n = sizeof(keys)/sizeof(keys[0]);
-    struct TrieNode *root = getNode();
-    rep(i, n) insert(root, keys[i]);
-    search(root, "the")? cout << "Yes\n": cout << "No\n";
-    search(root, "these")? cout << "Yes\n": cout << "No\n";   
-    return 0;
+int main() {
+  string keys[] = {"the", "a",   "there", "answer", "any",
+                   "by",  "bye", "their", "hey",    "hello"};
+  int n = sizeof(keys) / sizeof(keys[0]);
+  struct TrieNode *root = getNode();
+  rep(i, n) insert(root, keys[i]);
+  search(root, "the") ? cout << "Yes\n" : cout << "No\n";
+  search(root, "these") ? cout << "Yes\n" : cout << "No\n";
+  search(root, "hello") ? cout << "Yes\n" : cout << "No\n";
+  return 0;
 }
