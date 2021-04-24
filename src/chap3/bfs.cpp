5b19e8df8eab5b9934054d1ebe10dde30cad9c35
KoyanoBunsho <buna40950@gmail.com>
2021-04-24 19:47:27 +0900

    refactor

--- src/chap3/bfs.cpp
+++ src/chap3/bfs.cpp
@@ -1,31 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 using Graph = vector<vector<int>>;
-#define rep(i, N) for(int i=0; i<N; i++)
+#define rep(i, N) for (int i = 0; i < N; i++)
 
-int main(){
-    int N, M;
-    cin >> N >> M;
-    Graph G(N);
-    rep(i, M){
-        int a, b;
-        cin >> a >> b;
-        G[a].push_back(b);
-        G[b].push_back(a);
+int main() {
+  int N, M;
+  cin >> N >> M;
+  Graph G(N);
+  rep(i, M) {
+    int a, b;
+    cin >> a >> b;
+    G[a].push_back(b);
+    G[b].push_back(a);
+  }
+  vector<int> dist(N, -1);
+  queue<int> que;
+  dist[0] = 0;
+  que.push(0);
+  while (!que.empty()) {
+    int v = que.front();
+    que.pop();
+    for (int nv : G[v]) {
+      if (dist[nv] != -1)
+        continue;
+      dist[nv] = dist[v] + 1;
+      que.push(nv);
     }
-    vector<int> dist(N, -1);
-    queue<int> que;
-    dist[0] = 0;
-    que.push(0);
-    while(!que.empty()){
-        int v = que.front();
-        que.pop();
-        for (int nv:G[v]){
-            if (dist[nv]!=-1)continue;
-            dist[nv] = dist[v]+1;
-            que.push(nv);
-        }
-    }
-    rep(v, N) cout << v << ": " << dist[v] << endl;
-    return 0;
+  }
+  rep(v, N) cout << v << ": " << dist[v] << endl;
+  return 0;
 }
--- src/chap3/mergesort.cpp
+++ src/chap3/mergesort.cpp
@@ -1,56 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for(int i=0; i<n; i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-void merge(int arr[], int l, int m, int r){
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int L[n1], R[n2];
-    rep(i, n1) L[i] = arr[l+i];
-    rep(i, n2) R[i] = arr[m+1+i];
-    int i=0;
-    int j=0;
-    int k=l;
-    while(i<n1 && j<n2){
-        if(L[i]<=R[j]){
-            arr[k]=L[i];
-            i++;
-        }else{
-            arr[k]=R[j];
-            j++;
-        }
-        k++;
+void merge(int arr[], int l, int m, int r) {
+  int n1 = m - l + 1;
+  int n2 = r - m;
+  int L[n1], R[n2];
+  rep(i, n1) L[i] = arr[l + i];
+  rep(i, n2) R[i] = arr[m + 1 + i];
+  int i = 0;
+  int j = 0;
+  int k = l;
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
     }
-    while(i<n1){
-        arr[k]=L[i];
-        i++;
-        k++;
-    }
-    while(j<n2){
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+    k++;
+  }
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
 }
 
-void mergeSort(int arr[], int l, int r){
-    if(l>=r) return;
-    int m=l+(r-l)/2;
-    mergeSort(arr, l, m);
-    mergeSort(arr, m+1, r);
-    merge(arr, l, m, r);
+void mergeSort(int arr[], int l, int r) {
+  if (l >= r)
+    return;
+  int m = l + (r - l) / 2;
+  mergeSort(arr, l, m);
+  mergeSort(arr, m + 1, r);
+  merge(arr, l, m, r);
 }
 
-void printArray(int A[], int size){
-    rep(i, size) cout << A[i] << " ";
-    cout << endl;
+void printArray(int A[], int size) {
+  rep(i, size) cout << A[i] << " ";
+  cout << endl;
 }
 
-int main(){
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int arr_size = sizeof(arr)/sizeof(arr[0]);
-    printArray(arr, arr_size);
-    mergeSort(arr, 0, arr_size-1);
-    printArray(arr, arr_size);
-    return 0;
+int main() {
+  int arr[] = {12, 11, 13, 5, 6, 7};
+  int arr_size = sizeof(arr) / sizeof(arr[0]);
+  printArray(arr, arr_size);
+  mergeSort(arr, 0, arr_size - 1);
+  printArray(arr, arr_size);
+  return 0;
 }
