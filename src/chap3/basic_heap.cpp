2cf28f202848c0475c64c2698d44ebd43000da1a
KoyanoBunsho <buna40950@gmail.com>
2021-04-26 18:56:00 +0900

    refactor

--- src/chap3/basic_heap.cpp
+++ src/chap3/basic_heap.cpp
@@ -1,35 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define MAX 1000000
-#define rep(i, n) for(int i=0; i<n; i++)
+#define rep(i, n) for (int i = 0; i < n; i++)
 
-void heapify(int arr[], int n, int i){
-    int largest=i;
-    int l=2*i+1;
-    int r=2*i+2;
-    if(l<n && arr[l]>arr[largest]) largest=l;
-    if(r<n && arr[r]>arr[largest]) largest=r;
-    if(largest!=i){
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
+void heapify(int arr[], int n, int i) {
+  int largest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
+  if (l < n && arr[l] > arr[largest])
+    largest = l;
+  if (r < n && arr[r] > arr[largest])
+    largest = r;
+  if (largest != i) {
+    swap(arr[i], arr[largest]);
+    heapify(arr, n, largest);
+  }
 }
 
-void deleteRoot(int arr[], int& n){
-    int lastElement = arr[n-1];
-    arr[0]=lastElement;
-    n=n-1;
-    heapify(arr, n, 0);
+void deleteRoot(int arr[], int &n) {
+  int lastElement = arr[n - 1];
+  arr[0] = lastElement;
+  n = n - 1;
+  heapify(arr, n, 0);
 }
 
-void printArray(int arr[], int n){
-    rep(i, n) cout << arr[i] << endl;
-}
+void printArray(int arr[], int n) { rep(i, n) cout << arr[i] << endl; }
 
-int main(){
-    int arr[] = {10, 5, 3, 2, 4};
-    int n=sizeof(arr);
-    deleteRoot(arr, n);
-    printArray(arr, n);
-    return 0;
+int main() {
+  int arr[] = {10, 5, 3, 2, 4};
+  int n = sizeof(arr);
+  deleteRoot(arr, n);
+  printArray(arr, n);
+  return 0;
 }
