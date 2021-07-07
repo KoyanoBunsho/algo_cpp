114ea154c657f67ba99541272d87169f062088d8
KoyanoBunsho <buna40950@gmail.com>
2021-07-07 19:04:20 +0900

    refactor

--- src/joint.cpp
+++ src/joint.cpp
@@ -7,14 +7,17 @@ struct UnionFind {
   vector<int> sizes;
   UnionFind(int n) : par(n), sizes(n, 1) { rep(i, n) par[i] = i; }
   int find(int x) {
-    if (x == par[x]) return x;
+    if (x == par[x])
+      return x;
     return par[x] = find(par[x]);
   }
   void unite(int x, int y) {
     x = find(x);
     y = find(y);
-    if (x == y) return;
-    if (sizes[x] < sizes[y]) swap(x, y);
+    if (x == y)
+      return;
+    if (sizes[x] < sizes[y])
+      swap(x, y);
     par[y] = x;
     sizes[x] += sizes[y];
   }
@@ -22,31 +25,29 @@ struct UnionFind {
   int size(int x) { return sizes[find(x)]; }
 };
 
-int main(){
+int main() {
   int N, K, L;
   cin >> N >> K >> L;
   UnionFind uf_road(N);
   UnionFind uf_rail(N);
-  vector<int>p(K), q(K);
-  rep(i, K){
+  vector<int> p(K), q(K);
+  rep(i, K) {
     int a, b;
     cin >> a >> b;
-    a--; b--;
+    a--;
+    b--;
     uf_road.unite(a, b);
   };
   vector<int> r(L), s(L);
   rep(i, L) {
     int a, b;
     cin >> a >> b;
-    a--; b--;
+    a--;
+    b--;
     uf_rail.unite(a, b);
   };
   map<pair<int, int>, int> ma;
-  rep(i, N){
-    ma[make_pair(uf_road.find(i), uf_rail.find(i))]++;
-  }
-  rep(i, N){
-    cout << ma[make_pair(uf_road.find(i), uf_rail.find(i))] << endl;
-  }
+  rep(i, N) { ma[make_pair(uf_road.find(i), uf_rail.find(i))]++; }
+  rep(i, N) { cout << ma[make_pair(uf_road.find(i), uf_rail.find(i))] << endl; }
   return 0;
 }
